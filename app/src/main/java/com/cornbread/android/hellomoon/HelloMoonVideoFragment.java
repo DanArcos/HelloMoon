e3320b0b09cd20c4514831fb297514fe55dea094
DArcos <danarc0s7@gmail.com>
2015-02-15 19:21:14 -0500

    Added Video player with surface holder method.

---
+++ app/src/main/java/com/cornbread/android/hellomoon/HelloMoonVideoFragment.java
@@ -0,0 +1,109 @@
+package com.cornbread.android.hellomoon;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class HelloMoonVideoFragment extends Fragment {
+    private MediaPlayer mVideoPlayer;
+
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+
+    private Button mPlayButton;
+    private Button mStopButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon_video, parent, false);
+
+        mVideoPlayer = new MediaPlayer();
+
+        mSurfaceView = (SurfaceView) v.findViewById(R.id.surface_view);
+
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Log.d("TAG", "Surface created!");
+
+                //mSurfaceHolder = holder;
+
+                Log.d("TAG", (holder == null) + " is true if null");
+
+                mVideoPlayer.setDisplay(holder);
+                Log.d("TAG", "Set Display Created!");
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mVideoPlayer.stop();
+                mVideoPlayer.release();
+            }
+        });
+
+
+        mPlayButton = (Button) v.findViewById(R.id.hellomoon_playVideo);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Play Video
+                //Toast.makeText(getActivity(),"Play",Toast.LENGTH_SHORT).show();
+                play();
+            }
+        });
+
+        mStopButton = (Button) v.findViewById(R.id.hellomoon_stopVideo);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Stop Video
+                //Toast.makeText(getActivity(),"Stop",Toast.LENGTH_SHORT).show();
+                stop();
+            }
+        });
+
+        return v;
+    }
+
+    public void play(){
+
+        try {
+            mVideoPlayer.setDataSource(getActivity(), Uri.parse("android.resource://com.cornbread.android.hellomoon/raw/apollo17stroll"));
+            mVideoPlayer.prepare();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("TAG", "Prepare UnSuccessful");
+        }
+
+        mVideoPlayer.start();
+        Log.d("TAG", "Start Called");
+    }
+
+    public void stop() {
+        if (mVideoPlayer != null) {
+            mVideoPlayer.stop();
+            mVideoPlayer.release();
+            mVideoPlayer = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/cornbread/android/hellomoon/VideoPlayer.java
@@ -0,0 +1,68 @@
+package com.cornbread.android.hellomoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+public class VideoPlayer {
+    private MediaPlayer mPlayer;
+    private Boolean mPaused = false;
+
+
+    public void playFromScratch(Context c){
+        stop(); //This will destroy any other instances of the media player. We don't want two instances.
+
+        mPlayer = MediaPlayer.create(c, R.raw.apollo_17_stroll);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                stop(); // Call stop once the file is done playing
+            }
+        });
+        mPlayer.start();
+    }
+
+    public void PlayPause(Context c){
+
+        if (mPlayer  == null){
+            playFromScratch(c);
+            setPaused(false);
+        }
+        else{
+            if (mPlayer.isPlaying()){
+                pause();
+                setPaused(true);
+            }
+            else{
+                if (isPaused()){
+                    mPlayer.start();
+                    setPaused(false);
+                }
+                else{
+                    playFromScratch(c);
+                    setPaused(false);
+                }
+            }
+        }
+    }
+
+    public void pause(){
+        mPlayer.pause();
+    }
+
+    public void setPaused(Boolean paused){
+        mPaused = paused;
+    }
+
+    public Boolean isPaused(){
+        return mPaused;
+    }
+
+    public void stop(){
+        if (mPlayer != null){
+            mPlayer.release();
+            mPlayer = null;
+        }
+
+        setPaused(false);
+    }
+}
