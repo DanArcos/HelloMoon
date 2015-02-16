aa962da94912fc45486904f786be7984a442636e
DArcos <danarc0s7@gmail.com>
2015-02-15 20:08:43 -0500

    Added Play Pause and Stop button with full functionality.

--- app/src/main/java/com/cornbread/android/hellomoon/VideoViewPlayer.java
+++ app/src/main/java/com/cornbread/android/hellomoon/VideoViewPlayer.java
@@ -4,10 +4,19 @@ import android.app.Activity;
 import android.media.session.MediaController;
 import android.media.session.MediaSession;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 public class VideoViewPlayer extends Activity{
     VideoView mVideoView;
+    Button mPlayButton;
+    Button mPauseButton;
+    Button mStopButton;
+
+    Boolean isPaused = false;
+    Boolean isStopped = true;
+
     MediaController mMediaController;
 
     @Override
@@ -18,7 +27,45 @@ public class VideoViewPlayer extends Activity{
         mVideoView = (VideoView) findViewById(R.id.video_player_view);
         mVideoView.setVideoPath("android.resource://com.cornbread.android.hellomoon/raw/apollo17stroll");
 
-        mVideoView.start();
+        mPlayButton = (Button)findViewById(R.id.VideoView_Play);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isStopped){
+                    isStopped = false;
+                    mVideoView.start();
+                    mVideoView.resume();
+                    }
+
+                else{
+                    if(isPaused){
+                        isPaused = false;
+                        mVideoView.start();
+                    }
+                }
+
+
+            }
+        });
+
+        mPauseButton = (Button)findViewById(R.id.VideoView_Pause);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView.pause();
+                isPaused = true;
+            }
+        });
+
+        mStopButton = (Button)findViewById(R.id.VideoView_Stop);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isStopped = true;
+                mVideoView.stopPlayback();
+            }
+        });
 
 
     }
